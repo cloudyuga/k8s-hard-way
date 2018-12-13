8ff44eafe3a7fc4cbd698e42e81db7ff68c39f4d
vishalcloudyuga <vishal@cloudyuga.guru>
2018-12-13 16:09:20 +0530

    script

---
+++ scripts/script.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+terraform init
+terraform plan -var "pub_key=~/.ssh/id_rsa.pub" -var "pvt_key=~/.ssh/id_rsa"  -var "region=blr1" -var "ssh_fingerprint=$FINGERPRINT" -var "do_token=$TOKEN" -var "size=2gb" 
+terraform apply -input=false -auto-approve -var "pub_key=~/.ssh/id_rsa.pub" -var "pvt_key=~/.ssh/id_rsa"  -var "region=blr1" -var "ssh_fingerprint=$FINGERPRINT" -var "do_token=$TOKEN" -var "size=2gb" 
+
+exit 0
