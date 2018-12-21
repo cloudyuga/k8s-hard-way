14b76f1a0c209270f48e3e9d124dad97dd89a855
Vishal Ghule <vishal@cloudyuga.guru>
2018-12-21 11:49:43 +0530

    Update outputs.tf

--- 00-Setup/outputs.tf
+++ 00-Setup/outputs.tf
@@ -55,14 +55,14 @@ output "Worker2_PrivateIP_addresses" {
 }
 
 provisioner "local-exec" {
-    command = "MASTER_1_PUBLIC_IP == ${digitalocean_droplet.master-1.ipv4_address} >> ${terraform.workspace}.txt",
-    command = "MASTER_1_PRIVATE_IP == ${digitalocean_droplet.master-1.ipv4_address_private} >> ${terraform.workspace}.txt",
-    command = "MASTER_2_PUBLIC_IP == ${digitalocean_droplet.master-2.ipv4_address} >> ${terraform.workspace}.txt",
-    command = "MASTER_2_PRIVATE_IP == ${digitalocean_droplet.master-2.ipv4_address_private} >> ${terraform.workspace}.txt",
-    command = "WORKER_1_PUBLIC_IP == ${digitalocean_droplet.worker-1.ipv4_address} >> ${terraform.workspace}.txt",
-    command = "WORKER_1_PRIVATE_IP == ${digitalocean_droplet.worker-1.ipv4_address_private} >> ${terraform.workspace}.txt",
-    command = "WORKER_2_PUBLIC_IP == ${digitalocean_droplet.worker-2.ipv4_address} >> ${terraform.workspace}.txt",
-    command = "WORKER_2_PRIVATE_IP == ${digitalocean_droplet.worker-2.ipv4_address_private} >> ${terraform.workspace}.txt"
+    command = "MASTER_1_PUBLIC_IP=${digitalocean_droplet.master-1.ipv4_address} >> ${terraform.workspace}.txt",
+    command = "MASTER_1_PRIVATE_IP=${digitalocean_droplet.master-1.ipv4_address_private} >> ${terraform.workspace}.txt",
+    command = "MASTER_2_PUBLIC_IP=${digitalocean_droplet.master-2.ipv4_address} >> ${terraform.workspace}.txt",
+    command = "MASTER_2_PRIVATE_IP=${digitalocean_droplet.master-2.ipv4_address_private} >> ${terraform.workspace}.txt",
+    command = "WORKER_1_PUBLIC_IP=${digitalocean_droplet.worker-1.ipv4_address} >> ${terraform.workspace}.txt",
+    command = "WORKER_1_PRIVATE_IP=${digitalocean_droplet.worker-1.ipv4_address_private} >> ${terraform.workspace}.txt",
+    command = "WORKER_2_PUBLIC_IP=${digitalocean_droplet.worker-2.ipv4_address} >> ${terraform.workspace}.txt",
+    command = "WORKER_2_PRIVATE_IP=${digitalocean_droplet.worker-2.ipv4_address_private} >> ${terraform.workspace}.txt"
   }
 
 
